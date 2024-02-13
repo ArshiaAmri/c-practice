376803dfc9e2414ba5dd2b3a05338f9b5b776cef
ArshiaAmri <159038831+ArshiaAmri@users.noreply.github.com>
2024-02-13 11:24:07 +0330

    Create practice2.cpp

---
+++ practice2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << char(65) << char(82) << char(83) << char(72) << char(73) << char(65) << char(000) << char(65) << char(77)
+         << char(82) << char(73);
+    return 0;
+}
